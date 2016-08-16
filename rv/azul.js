43c0b84908c05440a04ef27c2cf69ed8ff09c2b3
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-16 15:40:28 -0500

    Update azul.js

--- rv/azul.js
+++ rv/azul.js
@@ -1,4 +1,4 @@
-var miCanvas = document.getElementById("miCanvas");
+var miCanvas = document.getElementById("miCanvas2");
 var contexto = miCanvas.getContext("2d");
-contexto.fillStyle = "red";
+contexto.fillStyle = "blue";
 contexto.fillRect( 150, 30, 50,50);
