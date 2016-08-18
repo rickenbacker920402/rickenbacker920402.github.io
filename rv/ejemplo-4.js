3855f1bf38bb30cc3cfab25c0238af9834772b6c
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-18 15:23:41 -0500

    Create ejemplo-4.js

---
+++ rv/ejemplo-4.js
@@ -0,0 +1,11 @@
+var escena = new THREE.Scene();
+var camara =  new THREE.PerpectiveCamera();
+camara.position.z = 5;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appenChild( renderizador.domElement);
+var forma  =  new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshNormalMaterial();
+var cubo = new THREE.Mesh(forma,material);
+cubo.rotateX(-Math.PI/4);
+cubo.rotatY(Math.PI/4);
