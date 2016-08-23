47a1a7dc3a9f07350be6fea6d044c06d65b5031f
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-23 15:28:01 -0500

    Create Ejemplo-Geometry.js

---
+++ rv/Ejemplo-Geometry.js
@@ -0,0 +1,29 @@
+var forma = new THREE.Geometry();
+forma.vertices.push( new THREE.Vector3( 1, 0, 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, 0, -1 ) );
+forma.vertices.push( new THREE.Vector3( -1, 0, -1 ) );
+forma.vertices.push( new THREE.Vector3( -1, 0, 1 ) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 0 ) );
+
+forma.faces.push( new THREE.Face3( 3, 2, 1) );
+forma.faces.push( new THREE.Face3( 3, 1, 0) );
+forma.faces.push( new THREE.Face3( 3, 0, 4) );
+forma.faces.push( new THREE.Face3( 0, 1, 4) );
+forma.faces.push( new THREE.Face3( 1, 2, 4) );
+forma.faces.push( new THREE.Face3( 2, 3, 4) );
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+var camara = new THREE.PerpectiveCamera();
+camara.position.z = 5;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeigth*.95);
+document.body.appenChild( renderizador.domElement );
+renderizador.render( escena, camara );
