b3532ba90fa4347a31b75980d527d669b4f74136
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-25 15:53:12 -0500

    Update revolucion.js

--- rv/revolucion.js
+++ rv/revolucion.js
@@ -11,6 +11,6 @@ escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,window.innerHeigth*.95);
+renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild( renderizador.domElement);
 renderizador,render( escena, camara);
