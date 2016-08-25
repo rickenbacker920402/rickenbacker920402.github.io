2696fb7a3d7c6e914df9f4c1959e0427fb4c4b7f
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-25 15:13:00 -0500

    Update EjemploFigura.js

--- rv/EjemploFigura.js
+++ rv/EjemploFigura.js
@@ -8,9 +8,9 @@ var malla = new THREE.Mesh(forma);
 var escena = new THREE.Scene();
 escena.add(malla);
 
-var camara = new THREE.PerpectiveCamera();
-camara.posistion.z = 100;
-var renderizador = new THREE.WebGLRender();
-renderizador.setSize( window.innerHeigth*.95, window.innerHeight*.95);
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
