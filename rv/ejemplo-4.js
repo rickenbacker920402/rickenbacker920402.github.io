d7489133f364e8ebbe748a68e85f0b1150e272a6
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-28 15:23:37 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -1,5 +1,5 @@
 var escena = new THREE.Scene();
-var camara =  new THREE.PerpectiveCamera();
+var camara =  new THREE.PerspectiveCamera();
 camara.position.z = 5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
