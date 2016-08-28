44fb2b953dc09a648cb7dd3c6c7fd049d9f3fa99
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-28 15:18:36 -0500

    Update Ejemplo-Geometry.js

--- rv/Ejemplo-Geometry.js
+++ rv/Ejemplo-Geometry.js
@@ -21,7 +21,7 @@ malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add( malla );
-var camara = new THREE.PerpectiveCamera();
+var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
