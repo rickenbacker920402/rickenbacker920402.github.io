84850458684a2b58d825a0624dcb7d6d6f3f35f3
rickenbacker920402 <r.ivan02@gmail.com>
2016-08-28 21:15:28 -0500

    Update Estrellita.js

--- rv/Estrellita.js
+++ rv/Estrellita.js
@@ -20,7 +20,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 200;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
