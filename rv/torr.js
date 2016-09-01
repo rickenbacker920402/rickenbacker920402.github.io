0891ed8477539a6d5a014cdea5929994fe754b1a
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-01 15:47:07 -0500

    Update torr.js

--- rv/torr.js
+++ rv/torr.js
@@ -22,7 +22,7 @@ var torreMalla = new THREE.Mesh(torreForma,material);
 var escena = new THREE.Scene();
 escena.add(torreMalla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=16;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
