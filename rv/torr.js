b5fcbc188e4bd5e9ff9791e4e4b5b06dee938df0
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 12:07:24 -0500

    Update torr.js

--- rv/torr.js
+++ rv/torr.js
@@ -1,10 +1,12 @@
-var base = new THREE.CylinderGeometry(0.5,0.5,.2);
-var base2 = new  THREE.CylinderGeometry(.35,.35,.05);
-base2.translate(0,1,0);
+var base = new THREE.CylinderGeometry(0.5, 0.5, 0.2);
+var base2 = new  THREE.CylinderGeometry(0.35, 0.35, 0.05);
+var base3 = new THREE.TorusBufferGeometry( 0.5, 2, 16, 100 );
+base2.translate(0, 1, 0);
+base3.translate(0, 0.2, 0);
 
 var baseMalla = new THREE.Mesh(base);
 var base2Malla = new THREE.Mesh(base2);
-
+var base3Malla = new THREE.Mesh(base3);
 
 
 
@@ -15,6 +17,7 @@ var torreForma = new THREE.Geometry();
 
 torreForma.merge(baseMalla.geometry,baseMalla.matrix);
 torreForma.merge(base2Malla.geometry,base2Malla.matrix);
+torreForma.merge(base3Malla.geometry,base3Malla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh(torreForma,material);
