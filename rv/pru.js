59f107266b632658ca4d7d138d2322f52742e85e
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 15:28:41 -0500

    Create pru.js

---
+++ rv/pru.js
@@ -0,0 +1,17 @@
+
+var figura = new THREE.RingGeometry( 0.7, 1, 30, 30 );
+
+var forma = new THREE.ExtrudeGeometry( figura, {amount: 10});
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/4);
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement);
+renderizador.render( escena, camara);
