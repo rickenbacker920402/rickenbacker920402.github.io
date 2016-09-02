21466e62a3b573b54482a6256cef2c7904698ed0
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 18:36:40 -0500

    Create cubo.js

---
+++ rv/cubo.js
@@ -0,0 +1,38 @@
+var forma = new THREE.Geometry();
+forma.vertices.push( new THREE.Vector3( 1, 1, 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, -1, 1 ) );
+forma.vertices.push( new THREE.Vector3( -1, 1, 1 ) );
+forma.vertices.push( new THREE.Vector3( -1, -1, 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, 1, -1 ) );
+forma.vertices.push( new THREE.Vector3( 1, -1, -1 ) );
+forma.vertices.push( new THREE.Vector3( -1, 1, -1) );
+forma.vertices.push( new THREE.Vector3( -1, -1, -1 ) );
+
+forma.faces.push( new THREE.Face3( 5, 7, 6) );
+forma.faces.push( new THREE.Face3( 8, 7, 6) );
+forma.faces.push( new THREE.Face3( 5, 1, 7) );
+forma.faces.push( new THREE.Face3( 7, 3, 5) );
+forma.faces.push( new THREE.Face3( 5, 1, 2) );
+forma.faces.push( new THREE.Face3( 5, 6, 1) );
+forma.faces.push( new THREE.Face3( 6, 8, 2) );
+forma.faces.push( new THREE.Face3( 8, 4, 6) );
+forma.faces.push( new THREE.Face3( 8, 7, 3) );
+forma.faces.push( new THREE.Face3( 8, 4, 3) );
+forma.faces.push( new THREE.Face3( 1, 2, 3) );
+forma.faces.push( new THREE.Face3( 4, 3, 2) );
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
