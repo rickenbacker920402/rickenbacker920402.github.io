d3010ee9a920b925936dc8c6f9cb80952bdcf05e
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 12:08:44 -0500

    Update torr.js

--- rv/torr.js
+++ rv/torr.js
@@ -1,6 +1,6 @@
 var base = new THREE.CylinderGeometry(0.5, 0.5, 0.2);
 var base2 = new  THREE.CylinderGeometry(0.35, 0.35, 0.05);
-var base3 = new THREE.TorusBufferGeometry( 0.5, 2, 16, 100 );
+var base3 = new THREE.TorusBufferGeometry( 0.5, 0.2, 16, 100 );
 base2.translate(0, 1, 0);
 base3.translate(0, 0.2, 0);
 
