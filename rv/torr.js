03ff9fae97fdbaa2bd6e3a4717d9c226276fbbdf
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 16:41:04 -0500

    Update torr.js

--- rv/torr.js
+++ rv/torr.js
@@ -4,6 +4,9 @@ var base3 = new  THREE.CylinderGeometry(0.35, 0.55, 1.5, 50, 25);
 var cima = new THREE.TorusGeometry( 0.35, 0.1, 16, 100 );
 var cima2 = new THREE.CylinderGeometry(0.55, 0.45, 0.2, 50, 25);
 var punta = new THREE.ConeGeometry( 0.15, 0.2, 32 );
+var punta1 = new THREE.ConeGeometry( 0.15, 0.2, 32 );
+var punta1 = new THREE.ConeGeometry( 0.15, 0.2, 32 );
+var punta3 = new THREE.ConeGeometry( 0.15, 0.2, 32 );
 
 
 base2.rotateX(Math.PI/2);
@@ -15,7 +18,10 @@ base2.translate(0, -0.9, 0);
 base3.translate(0, 0.05, 0);
 cima.translate(0, 0.8, 0);
 cima2.translate(0, 0.9, 0);
-punta.translate(0, 1.2, 0.55);
+punta.translate(0, 1.2, 0.5);
+punta1.translate(0, 1.2, -0.5);
+punta2.translate(0.5, 1.2, 0);
+punta3.translate(-0.5, 1.2, 0);
 
 
 var baseMalla = new THREE.Mesh(base);
@@ -24,6 +30,9 @@ var base3Malla = new THREE.Mesh(base3);
 var cimaMalla = new THREE.Mesh(cima);
 var cima2Malla = new THREE.Mesh(cima2);
 var puntaMalla = new THREE.Mesh(punta);
+var punta1Malla = new THREE.Mesh(punta1);
+var punta2Malla = new THREE.Mesh(punta2);
+var punta3Malla = new THREE.Mesh(punta3);
 
 
 var torreForma = new THREE.Geometry();
@@ -34,6 +43,9 @@ torreForma.merge(base3Malla.geometry,base3Malla.matrix);
 torreForma.merge(cimaMalla.geometry,cimaMalla.matrix);
 torreForma.merge(cima2Malla.geometry,cima2Malla.matrix);
 torreForma.merge(puntaMalla.geometry,puntaMalla.matrix);
+torreForma.merge(punta1Malla.geometry,punta1Malla.matrix);
+torreForma.merge(punta2Malla.geometry,punta2Malla.matrix);
+torreForma.merge(punta3Malla.geometry,punta3Malla.matrix);
 
 
 var material = new THREE.MeshNormalMaterial();
