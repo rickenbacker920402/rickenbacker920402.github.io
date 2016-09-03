3a4caafc7e58bcd55c05f7f8dafd2a54b6d0a474
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 19:08:22 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -26,7 +26,7 @@ forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI);
+malla.rotateX(Math.PI/4);
 malla.rotateY(Math.PI);
 
 var escena = new THREE.Scene();
