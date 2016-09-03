f6cff0c419bfdf3f61924c473e85970fbf9fa619
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-02 19:11:22 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -32,7 +32,7 @@ malla.rotateY(Math.PI/4);
 var escena = new THREE.Scene();
 escena.add( malla );
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 10;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild( renderizador.domElement );
