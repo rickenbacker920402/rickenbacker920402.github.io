a1deb9f4bc83d9eb65f1010228418849f2bfc03a
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-06 15:44:22 -0500

    Update ejemploperspectiva.js

--- rv/ejemploperspectiva.js
+++ rv/ejemploperspectiva.js
@@ -16,7 +16,7 @@ esfera1.position.x = 5;
 var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 1 ),
                               new THREE.MeshNormalMaterial());
 esfera2.position.x = 5;
-esfera.position.z = -10;
+esfera2.position.z = -10;
 var escena = new THREE.Scene();
 escena.add(esfera1);
 escena.add(esfera2);
