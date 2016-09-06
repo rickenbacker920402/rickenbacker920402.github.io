c40810ae650b3c79e6bc8916723bd0d39cf7f5b7
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-06 14:57:55 -0500

    Create ejemploperspectiva.js

---
+++ rv/ejemploperspectiva.js
@@ -0,0 +1,27 @@
+var campoVision = 45; //grados
+var relacionAspecto = window.innerWidth / window.innerHeigth;
+var planoCercano =1;
+var planoLejano = 1000;
+var camara = new THREE.PerspectiveCamera(campoVision,
+                                          relacionAspecto,
+                                          planoCercano,
+                                          planoLejano);
+camara.position.z = 15;
+var cubo = new THREE.Mesh (new THREE.Boxgeo,etry( 2, 2, 2),
+                          new THREE.MeshNormalMaterial());
+cubo.rotateX(Math.PI/4);
+var esfera1 = new THREE.Mesh(new THREE.SphereGeometry(1 ),
+                              new THREE.MeshNormalMaterial());
+esfera1.position.x = 5;
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 1 ),
+                              new THREE.MeshNormalMaterial());
+esfera2.position.x = 5;
+esfera.position.z = -10;
+var escena = new THREE.Scene();
+escena.add(esfera1);
+escena.add(esfera2);
+escena.add(cubo);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerWidth, window.innerHeigth);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
