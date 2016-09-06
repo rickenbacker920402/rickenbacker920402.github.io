9ed69cf5f60c76c1b73d47238d566d454bdb079f
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-06 15:33:28 -0500

    Update camara-ortografica.js

--- rv/camara-ortografica.js
+++ rv/camara-ortografica.js
@@ -1,7 +1,7 @@
 var camara = new THREE.OrthographicCamera();
 
-camara.left = window.innerHeight /-2;
-camara.right = window.innerHeight /2;
+camara.left = window.innerWidth /-2;
+camara.right = window.innerWidth /2;
 camara.top = window.innerHeight /2;
 camara.bottom = window.innerHeight /-2;
 camara.near = 0.1;
@@ -11,7 +11,7 @@ camara.position.z =100;
 
 var cubo = new THREE.Mesh( new THREE.BoxGeometry( 30, 30, 30 ), new THREE.MeshNormalMaterial() );
 cubo.rotateY(Math.PI/4);
-var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMaterial() );
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 30 ), new THREE.MeshNormalMaterial() );
 esfera1.position.x = 50;
 var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMaterial() );
 esfera2.position.x = -50;
