f3cea8427dd40e31b441ce96414f60ea58bf3ba6
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-13 15:07:29 -0500

    Create ejemplo-lambertMaterial.js

---
+++ rv/ejemplo-lambertMaterial.js
@@ -0,0 +1,20 @@
+var luzPuntual = new THREE.PointLight(0xFFFFFF);
+luzPuntual.position.x = 10;
+luzPuntual.position.y = 10;
+luzPuntual.position.z = 10;
+
+
+
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial( {color: '#00cc00'});
+var malla = new THREE.Mesh( forma, material);
+
+var escena = new THREE.Scene();
+escena.add(malla);
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var lienzo = document.getElemtById("luzPuntual");
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: ture});
+renderizador.setSize(600,600);
+renderizador.render(escena, camara);
