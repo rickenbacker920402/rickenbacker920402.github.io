b5d772aed493382eab93cf0f21993e72eb13c278
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-13 15:25:00 -0500

    Create luz-ambiental.js

---
+++ rv/luz-ambiental.js
@@ -0,0 +1,17 @@
+var iluminacion = new THREE.AmbientLight(0xFFFFFF);
+
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial({color: "#00cc00"});
+malla = new THREE.Mesh( forma, material);
+
+var escena = new THREE.Scena();
+escena.add(malla);
+escena.add(iluminacion);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var lienzo = document.getElementById("luzAmbiental");
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+renderizador.setSize( 600,600);
+renderizador.render(escena, camara);
