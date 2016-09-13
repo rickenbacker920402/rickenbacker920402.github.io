da1c83a514c72aa22ab8db0ccb2bf5b06a98e9f1
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-13 15:12:51 -0500

    Update ejemplo-lambertMaterial.js

--- rv/ejemplo-lambertMaterial.js
+++ rv/ejemplo-lambertMaterial.js
@@ -15,6 +15,6 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
 var lienzo = document.getElementById("lambertMaterial");
-var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: ture});
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
 renderizador.setSize(600,600);
 renderizador.render(escena, camara);
