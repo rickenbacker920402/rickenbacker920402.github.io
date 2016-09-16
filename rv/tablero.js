c08c054b043e4d91c94e2477460cda8a7a73c91c
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-15 23:58:38 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -70,7 +70,7 @@ for(var f=1; f<=8; f++)
 }
 
 escena.add(mallaTablero);
-var lienzo = document.getElementById("basicMaterial");
-var renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true});
-renderizador.setSize( 600, 600);
-renderizador.render(escena,camara);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerWidth, window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
