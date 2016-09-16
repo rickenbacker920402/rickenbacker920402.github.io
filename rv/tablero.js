4392c27b4da47dd205715da2f4ddfdc1d9e479e9
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-16 00:04:44 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -7,8 +7,8 @@ var camara = new THREE.PerspectiveCamera(campoVision,
                                           planoCercano,
                                           planoLejano);
 camara.position.z = 150;
-camara.position.y = -150;
-camara.position.x = -150;
+camara.position.y = -100;
+camara.position.x = 100;
 camara.lookAt(new THREE.Vector3(45,45,0));
 
 
