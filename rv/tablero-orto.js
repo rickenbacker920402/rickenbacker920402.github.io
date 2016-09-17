418a7779a3e0a01d396070f76877b29dd753baef
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-16 20:01:48 -0500

    Update tablero-orto.js

--- rv/tablero-orto.js
+++ rv/tablero-orto.js
@@ -109,6 +109,21 @@ for(var f=1; f<=8; f++)
     }
   }
 }
+casillaNegra[5].castShadow = true;
+casillaNegra[13].castShadow = true;
+casillaNegra[21].castShadow = true;
+casillaNegra[29].castShadow = true;
+casillaNegra[30].castShadow = true;
+casillaNegra[31].castShadow = true;
+casillaNegra[32].castShadow = true;
+casillaBlanca[1].castShadow = true;
+casillaBlanca[9].castShadow = true;
+casillaBlanca[17].castShadow = true;
+casillaBlanca[25].castShadow = true;
+casillaBlanca[29].castShadow = true;
+casillaBlanca[30].castShadow = true;
+casillaBlanca[31].castShadow = true;
+casillaBlanca[32].castShadow = true;
 
 escena.add(mallaTablero);
 escena.add(torreMalla);
