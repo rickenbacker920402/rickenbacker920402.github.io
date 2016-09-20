8cb5b02f45d52f9dccefac9c38b3983427bdefc0
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-20 15:10:57 -0500

    Create tablero2.js

---
+++ rv/tablero2.js
@@ -0,0 +1,154 @@
+//Iluminacion
+var iluminacion = new THREE.PointLight(0xFF00FF);
+iluminacion.position.z = -120;
+iluminacion.position.x = -45;
+iluminacion.position.y = -45;
+
+var iluminacion1 = new THREE.PointLight(0x00FFFF);
+iluminacion1.position.z = -120;
+iluminacion1.position.x = 145;
+iluminacion1.position.y = 145;
+
+var iluminacion2 = new THREE.PointLight(0xFFFF00);
+iluminacion2.position.z = -120;
+iluminacion2.position.x = 145;
+iluminacion2.position.y = -45;
+//Perspective camera
+var campoVision = 45; //grados
+var relacionAspecto = window.innerWidth / window.innerHeight;
+var planoCercano =100;
+var planoLejano = 1000;
+var camara = new THREE.PerspectiveCamera(campoVision,
+                                          relacionAspecto,
+                                          planoCercano,
+                                          planoLejano);
+camara.position.z = -150;
+camara.position.x = 45;
+camara.position.y = -50;
+camara.lookAt(new THREE.Vector3(45,45,0));
+
+
+//Torre 1
+var torreMalla = new THREE.Mesh(torreForma,material);
+torreMalla.rotateX(Math.PI*3/2);
+torreMalla.scale.set(5,5,7);
+torreMalla.position.set(10,10,-10);
+
+//Torre 2
+var torreMalla1 = new THREE.Mesh(torreForma,material2);
+torreMalla1.rotateX(Math.PI*3/2);
+torreMalla1.scale.set(5,5,7);
+torreMalla1.position.set(10,80,-10);
+
+//Torre 3
+var torreMalla2 = new THREE.Mesh(torreForma,material);
+torreMalla2.rotateX(Math.PI*3/2);
+torreMalla2.scale.set(5,5,7);
+torreMalla2.position.set(80,10,-10);
+
+//Torre 4
+var torreMalla3 = new THREE.Mesh(torreForma,material2);
+torreMalla3.rotateX(Math.PI*3/2);
+torreMalla3.scale.set(5,5,7);
+torreMalla3.position.set(80,80,-10);
+
+//Tablero
+
+var casillaBlanca = new Array();
+var casillaNegra = new Array();
+
+var box = new THREE.BoxGeometry( 10, 10, 10 ,10,10,10);
+var mboxBlanca = new THREE.MeshLambertMaterial({color : 0x999a9b });
+var mboxNegra =  new THREE.MeshLambertMaterial({color : 0x6b6b6b });
+var bordeTablero = new THREE.BoxGeometry( 100, 100, 7,10,10,10 );
+var mbTablero = new THREE.MeshLambertMaterial({color : 0x644703 });
+var mallaTablero = new THREE.Mesh(bordeTablero, mbTablero);
+mallaTablero.position.set(45,45,0);
+var n=1;
+var b=1;
+var escena = new THREE.Scene();
+
+for(var i=1; i<=32; i++){
+casillaBlanca[i] = new THREE.Mesh(box, mboxBlanca);
+casillaNegra[i] = new THREE.Mesh(box, mboxNegra);
+}
+
+for(var f=1; f<=8; f++)
+{
+  for(var c=1; c<=8; c++)
+  {
+    if(f%2==0)
+    {
+      if(c%2==0)
+      {
+       casillaNegra[n].position.set((f*10),(c*10),0);
+       escena.add(casillaNegra[n]);
+       //casillaNegra[n].castShadow = true;
+       casillaNegra[n].receiveShadow = true;
+       n=n+1;
+      }
+      else
+      {
+       casillaBlanca[b].position.set((f*10),(c*10),0);
+       escena.add(casillaBlanca[b]);
+       //casillaBlanca[b].castShadow = true;
+       casillaBlanca[b].receiveShadow = true;
+       b=b+1;
+      }
+    }
+    else
+    {
+      if(c%2==0)
+      {
+      casillaBlanca[b].position.set((f*10),(c*10),0);
+      escena.add(casillaBlanca[b]);
+      //casillaBlanca[b].castShadow = true;
+      casillaBlanca[b].receiveShadow = true;
+      b=b+1;
+      }
+      else
+      {
+      casillaNegra[n].position.set((f*10),(c*10),0);
+      escena.add(casillaNegra[n]);
+      //casillaNegra[n].castShadow = true;
+      casillaNegra[n].receiveShadow = true;
+      n=n+1;
+      }
+    }
+  }
+}
+
+casillaNegra[8].castShadow = true;
+casillaNegra[16].castShadow = true;
+casillaNegra[24].castShadow = true;
+casillaNegra[29].castShadow = true;
+casillaNegra[30].castShadow = true;
+casillaNegra[31].castShadow = true;
+casillaNegra[32].castShadow = true;
+casillaBlanca[4].castShadow = true;
+casillaBlanca[12].castShadow = true;
+casillaBlanca[20].castShadow = true;
+casillaBlanca[28].castShadow = true;
+casillaBlanca[29].castShadow = true;
+casillaBlanca[30].castShadow = true;
+casillaBlanca[31].castShadow = true;
+casillaBlanca[32].castShadow = true;
+
+escena.add(mallaTablero);
+escena.add(torreMalla);
+escena.add(torreMalla1);
+escena.add(torreMalla2);
+escena.add(torreMalla3);
+escena.add(iluminacion);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerWidth, window.innerHeight);
+renderizador.shadowMapEnabled = true;
+document.body.appendChild(renderizador.domElement);
+mallaTablero.castShadow = true;
+mallaTablero.receiveShadow = true;
+torreMalla.castShadow = true;
+torreMalla1.castShadow = true;
+torreMalla2.castShadow = true;
+torreMalla3.castShadow = true;
+iluminacion.castShadow = true;
+renderizador.render(escena, camara);
