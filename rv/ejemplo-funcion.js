5fcb82fb397a8136720ffcfa5b2cb6a132dc49e8
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-22 14:59:28 -0500

    Create ejemplo-funcion.js

---
+++ rv/ejemplo-funcion.js
@@ -0,0 +1,13 @@
+function init(){
+var malla(new THREE.BoxGeometry(p,p,p), new THREE,MeshNormalMaterial());
+escena = new THREE.Scene();
+escena.add(malla);
+camara = new THREE.PerspesctiveCamera();
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(700,700);
+document.body.appendChild(renderizador.domElement);
+
+}
+var main = function(p){
+renderizador.render(escena, camara);
+}
