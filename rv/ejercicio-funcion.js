2f22c960b68ab9d5c87e778b2305b4c76bad7a7b
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-22 15:43:27 -0500

    Update ejercicio-funcion.js

--- rv/ejercicio-funcion.js
+++ rv/ejercicio-funcion.js
@@ -4,6 +4,7 @@ escena = new THREE.Scene();
 escena.add(malla);
 camara = new THREE.PerspectiveCamera();
 camara.position.z=5*p;
+step=0.1;
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(700,700);
 document.body.appendChild(renderizador.domElement);
@@ -15,8 +16,12 @@ var loop = function(){
 //  p(1);......2
 renderizador.render(escena, camara);
 malla.rotateY(0.01);
+if (Math.abs(malla.position.x)<5)
+step = -step;
+malla.position.x+=step;
+
 }
-var escena,camara,renderizador;
+var escena,camara,renderizador,step;
 //init(1);.......1
 //main(2);.......1
 //main(init);.......2
