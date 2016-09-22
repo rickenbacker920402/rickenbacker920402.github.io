4069e6e7b896ff71f05f4e3dc4f3d79991c58874
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-22 15:26:04 -0500

    Update ejemplo-funcion.js

--- rv/ejemplo-funcion.js
+++ rv/ejemplo-funcion.js
@@ -1,5 +1,5 @@
 function init(p){
-var malla = new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
+malla = new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
 escena = new THREE.Scene();
 escena.add(malla);
 camara = new THREE.PerspectiveCamera();
@@ -9,11 +9,18 @@ renderizador.setSize(700,700);
 document.body.appendChild(renderizador.domElement);
 
 }
-var main = function(p){
-  p(1);
+var loop = function(){
+  requestAnimationFrame(loop);
+  
+}
+//var main = function(p){......1
+//  p(1);......2
 renderizador.render(escena, camara);
+malla.rotateY(0.01);
 }
 var escena,camara,renderizador;
-//init(1);
-//main(2);
-main(init);
+//init(1);.......1
+//main(2);.......1
+//main(init);.......2
+init(1);
+loop();
