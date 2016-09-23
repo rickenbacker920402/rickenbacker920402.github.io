6982a5be741de52cd442b481a2d95122a7428c97
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-22 20:32:54 -0500

    Update tablero2.js

--- rv/tablero2.js
+++ rv/tablero2.js
@@ -125,21 +125,6 @@ for(var f=1; f<=8; f++)
   }
 }
 
-casillaNegra[8].castShadow = true;
-casillaNegra[16].castShadow = true;
-casillaNegra[24].castShadow = true;
-casillaNegra[29].castShadow = true;
-casillaNegra[30].castShadow = true;
-casillaNegra[31].castShadow = true;
-casillaNegra[32].castShadow = true;
-casillaBlanca[4].castShadow = true;
-casillaBlanca[12].castShadow = true;
-casillaBlanca[20].castShadow = true;
-casillaBlanca[28].castShadow = true;
-casillaBlanca[29].castShadow = true;
-casillaBlanca[30].castShadow = true;
-casillaBlanca[31].castShadow = true;
-casillaBlanca[32].castShadow = true;
 
 escena.add(mallaTablero);
 escena.add(torreMalla);
