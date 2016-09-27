bc345754b3b34e178b6f558793262bc451df8728
rickenbacker920402 <r.ivan02@gmail.com>
2016-09-27 14:57:46 -0500

    Create ajuste-ventana.js

---
+++ rv/ajuste-ventana.js
@@ -0,0 +1,30 @@
+var VENTANA = new Object();
+VENTANA.listener = function(){
+VENTANA.camara.aspect = window.innerWidth/ window.innerHeight;
+VENTANA.camara.updateProjectionMatrix();
+VENTANA.renderizador.setSize(window.innerWidth, window.innerHeight);
+}
+
+VENTANA.setup = function(){
+var tipo-evento = 'resize';
+var capturarp = false;
+window.addEventListener( tipo-evento, VENTANA.listener, capturarp);
+VENTANA.escena = new THREE.Scene();
+VENTANA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth /window. innerHeight, 0.1, 1000);
+VENTANA.camara.position.z = 5;
+var lienzo = document.getElemtById("ejemplo-ventana");
+VENTANA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+VENTANA.renderizador.setSize( window.innerWidth, window.innerHeight);
+VENTANA.malla = new THREE.Mesh(NEW THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
+VENTANA.escena.add(VENTANA.malla);
+}
+
+VENTANA.loop = function(){
+requestAnimationFrame(VENTANA.loop);
+VENTANA.malla.rotateX(0.001);
+VENTANA.malla.rotateY(0.001);
+VENTANA.renderizador.render(VENTANA.escena, VENTANA.camara);
+}
+
+VENTANA.setup();
+VENTANA.loop();
