1b4d38bbe039c236cc1e28a61d561a5f1255f480
rickenbacker920402 <r.ivan02@gmail.com>
2016-10-04 15:57:09 -0500

    Create herencia.js

---
+++ rv/herencia.js
@@ -0,0 +1,45 @@
+var PROTOTIPO = new Object();
+
+PROTOTIPO.ArbolGeometry = function(){
+THREE.Geometry.call(this);
+
+var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
+var esferaForma = new THREE.SphereGeometry(.65);
+esferaForma.translate(0,1,0);
+
+var troncoMalla = new THREE.Mesh(troncoForma);
+varvar esferaMalla = new THREE.Mesh(esferaForma);
+
+this.merge(troncoMalla.geometry, troncoMalla.matrix);
+this.merge(esferaMalla.geometry, esferaMalla.matrix);
+}
+
+PROTOTIPO.ArbolGeometry.prototype = new THREE.Geometry();
+
+PROTOTIPO.setup = function(){
+var arbol1 = new THREE.Mesh( new PROTOTIPO.ArbolGeometry(), new THREE.MeshNormalMaterial());
+var arbol2 = new THREE.Mesh( new PROTOTIPO.arbolGeometry(), new THREE.MeshNormalMaterial());
+
+arbol1.position.x=5;
+arbol2.position.x=-5;
+
+PROTOTIPO.camara = new THREE.PerspectiveCamera();
+PROTOTIPO.camara.position.z = 20;
+
+var lienzo = document.getElementById("ejemplo-prototipo");
+PROTOTIPO.renderizador ]= new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+PROTOTIPO.renderizador.setSize(600,600);
+
+PROTOTIPO.escena = new THREE.Scene();
+PROTOTIPO.escena.add(arbol1);
+PROTOTIPO.escena.add(arbol2);
+}
+
+PROTOTIPO.loop = fucntion(){
+requesAnimationFrame(PROTOTIPO.loop);
+PROTOTIPO.renderizador.render(PROTOTIPO.escena,PROTOTIPO.camara);
+}
+
+PROTOTIPO.setup();
+PROTOTIPO.loop();
+
