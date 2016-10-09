e5073446fa125fc3109661fc3fa830a5e72b7caf
rickenbacker920402 <r.ivan02@gmail.com>
2016-10-08 20:35:15 -0500

    Update tablero-tex.js

--- rv/tablero-tex.js
+++ rv/tablero-tex.js
@@ -8,10 +8,9 @@ AJEDREZ.luzPuntual.position.z = -45;
 //
 AJEDREZ.retrollamada = function (textura1){
 material = new THREE.MeshLambertMaterial({ map: textura1} );
-AJEDREZ.malla = new THREE.Mesh( new THREE.BoxGeometry(10,10,10,10,10,10), material);
 AJEDREZ.casillaB = new Array();
 for(var i=1; i<=32; i++){
-AJEDREZ.casillaB[i] = AJEDREZ.malla;
+AJEDREZ.casillaB[i] = new THREE.Mesh( new THREE.BoxGeometry(10,10,10,10,10,10), material);
 //AJEDREZ.escena.add(AJEDREZ.casillaB[i]);
 }
 var  b=1;
@@ -48,10 +47,9 @@ var  b=1;
 
 AJEDREZ.retrollamada1 = function (textura2){
 var material1 = new THREE.MeshLambertMaterial({ map: textura2} );
-AJEDREZ.malla1 = new THREE.Mesh( new THREE.BoxGeometry(10,10,10,10,10,10), material1);
 AJEDREZ.casillaN = new Array();
 for(var i=1; i<=32; i++){
-AJEDREZ.casillaN[i] = AJEDREZ.malla1;
+AJEDREZ.casillaN[i] = new THREE.Mesh( new THREE.BoxGeometry(10,10,10,10,10,10), material1);
 //AJEDREZ.escena.add(AJEDREZ.casillaN[i]);
 }
 var  n=1;
