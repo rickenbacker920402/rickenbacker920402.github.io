b7db53a3dfdb5e9d8033452a7ffcd2e804f7db22
rickenbacker920402 <r.ivan02@gmail.com>
2016-10-15 05:23:38 -0500

    Update Peon.js

--- rv/Peon.js
+++ rv/Peon.js
@@ -1,17 +1,21 @@
 var base = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25);
 var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
+var base3 = new  THREE.CylinderGeometry(0.35, 0.55, 1.5, 50, 25);
 
 base2.rotateX(Math.PI/2);
 base.translate(0, -1, 0);
 base2.translate(0, -0.9, 0);
+base3.translate(0, 0.05, 0);
 
 var baseMalla = new THREE.Mesh(base);
 var base2Malla = new THREE.Mesh(base2);
+var base3Malla = new THREE.Mesh(base3);
 
 var peonForma = new THREE.Geometry();
 
 peonForma.merge(baseMalla.geometry,baseMalla.matrix);
 peonForma.merge(base2Malla.geometry,base2Malla.matrix);
+peonForma.merge(base3Malla.geometry,base3Malla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var peonMalla = new THREE.Mesh(peonForma,material);
