7eb7bcfd2eb6548e1938a013868425f38d441051
rickenbacker920402 <r.ivan02@gmail.com>
2016-10-15 04:14:11 -0500

    Update tablero-c.js

--- rv/tablero-c.js
+++ rv/tablero-c.js
@@ -1,4 +1,5 @@
 var AJEDREZ = new Object();
+
 AJEDREZ.luzPuntual = new THREE.PointLight(0xFFFFFF);
 AJEDREZ.luzPuntual.position.x = -120;
 AJEDREZ.luzPuntual.position.y = -45;
@@ -17,10 +18,10 @@ AJEDREZ.renderizador.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(AJEDREZ.renderizador.domElement);
 AJEDREZ.renderizador.showMapEnabled = true;
 
-AJEDREZ.material =new THREE.MeshLambertMaterial({ map: textura1} );
-
 //Torre Forma
-function Torre(){
+AJEDREZ.Casilla = function(){
+ this.casillaF = new THREE.BoxGeometry(10,10,10,10,10,10);
+}
 var base = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25);
 var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
 var base3 = new  THREE.CylinderGeometry(0.35, 0.55, 1.5, 50, 25);
@@ -31,6 +32,8 @@ var punta1 = new THREE.ConeGeometry( 0.15, 0.2, 32 );
 var punta2 = new THREE.ConeGeometry( 0.15, 0.2, 32 );
 var punta3 = new THREE.ConeGeometry( 0.15, 0.2, 32 );
 var anillo = new THREE.TorusGeometry( 0.4, 0.1, 16, 100 );
+
+
 
 base2.rotateX(Math.PI/2);
 cima.rotateX(Math.PI/2);
@@ -74,17 +77,100 @@ torreForma.merge(punta2Malla.geometry,punta2Malla.matrix);
 torreForma.merge(punta3Malla.geometry,punta3Malla.matrix);
 torreForma.merge(anilloMalla.geometry,anilloMalla.matrix);
 
-torreForma.rotateX(Math.PI*3/2);
-torreForma.scale.set(5,5,7);
-
-this.malla = new THREE.Mesh(torreForma, AJEDREZ.material);
+//casillas blancas
+AJEDREZ.retrollamada = function (textura1){
+material = new THREE.MeshLambertMaterial({ map: textura1} );
+AJEDREZ.casillaB = new Array();
+for(var i=1; i<=32; i++){
+AJEDREZ.casillaB[i] = new AJEDREZ.Casilla();
+}
+var  b=1;
+ for(var f=1; f<=8; f++)
+{
+  for(var c=1; c<=8; c++)
+  {
+    if(f%2==0)
+    {
+      if(c%2==0){
+       b=b;
+      }
+       else
+      {
+       AJEDREZ.casillaB[b].position.set((f*10),(c*10),0);
+       AJEDREZ.escena.add(AJEDREZ.casillaB[b]);
+       AJEDREZ.casillaB[b].receiveShadow = true;
+       b=b+1;
+      }
+    }
+    else
+    {
+      if(c%2==0)
+      {
+      AJEDREZ.casillaB[b].position.set((f*10),(c*10),0);
+      AJEDREZ.escena.add(AJEDREZ.casillaB[b]);
+      AJEDREZ.casillaB[b].receiveShadow = true;
+      b=b+1;
+      }
+    }
+  }
+}
+ 
+  
 }
 
-AJEDREZ.retrollamada = function (textura1){
+AJEDREZ.retrollamada1 = function (textura2){
+var material1 = new THREE.MeshLambertMaterial({ map: textura2} );
+AJEDREZ.casillaN = new Array();
+for(var i=1; i<=32; i++){
+AJEDREZ.casillaN[i] = new THREE.Mesh( new THREE.BoxGeometry(10,10,10,10,10,10), material1);
 
+}
+var  n=1;
+  for(var x=1; x<=8; x++)
+{
+  for(var z=1; z<=8; z++)
+  {
+    if(x%2==0)
+    {
+      if(z%2==0)
+      {
+       AJEDREZ.casillaN[n].position.set((x*10),(z*10),0);
+       AJEDREZ.escena.add(AJEDREZ.casillaN[n]);
+       AJEDREZ.casillaN[n].receiveShadow = true;
+       
+       n=n+1;
+      }
+    }
+    else
+    {
+      if(z%2==0){
+       n=n
+      }
+       else
+      {
+     AJEDREZ.casillaN[n].position.set((x*10),(z*10),0);
+     AJEDREZ.escena.add(AJEDREZ.casillaN[n]);
+     AJEDREZ.casillaN[n].receiveShadow = true;
+      n=n+1;
+      }
+    }
+  }
+}
+}
+
+AJEDREZ.retrollamada2 = function (textura3){
+var material2 = new THREE.MeshLambertMaterial({ map: textura3} );
+AJEDREZ.malla2 = new THREE.Mesh( new THREE.BoxGeometry(100,100,7,10,10,10), material2);
+AJEDREZ.malla2.position.set(45,45,0);
+AJEDREZ.escena.add(AJEDREZ.malla2);
+AJEDREZ.malla2.receiveShadow = true;
+}
+
+AJEDREZ.retrollamada3 = function (textura4){
+var material3 = new THREE.MeshLambertMaterial({map : textura4});
  AJEDREZ.torreB = new Array();
  for (var i=1;i<=2;i++){
-  AJEDREZ.torreB[i] = new Torre();
+  AJEDREZ.torreB[i] = new THREE.Mesh( torreForma, material3);
   AJEDREZ.torreB[i].rotateX(Math.PI*3/2);
   AJEDREZ.torreB[i].scale.set(5,5,7);
   AJEDREZ.escena.add(AJEDREZ.torreB[i]);
@@ -95,14 +181,38 @@ AJEDREZ.retrollamada = function (textura1){
  AJEDREZ.torreB[2].position.set(10,80,-10);
 }
 
+AJEDREZ.retrollamada4 = function (textura5){
+var material4 = new THREE.MeshLambertMaterial({map : textura5});
+ AJEDREZ.torreN = new Array();
+ for (var i=1;i<=2;i++){
+  AJEDREZ.torreN[i] = new THREE.Mesh( torreForma, material4);
+  AJEDREZ.torreN[i].rotateX(Math.PI*3/2);
+  AJEDREZ.torreN[i].scale.set(5,5,7);
+  AJEDREZ.escena.add(AJEDREZ.torreN[i]); 
+  AJEDREZ.torreN[i].castShadow = true;
+  AJEDREZ.torreN[i].receiveShadow = true;
+ }
+ AJEDREZ.torreN[1].position.set(80,10,-10);
+ AJEDREZ.torreN[2].position.set(80,80,-10);
+}
+
 AJEDREZ.setup = function(){
 AJEDREZ.escena = new THREE.Scene();
 
 var cargador = new THREE.TextureLoader();
   cargador.load("MarmolBlanco.jpg", AJEDREZ.retrollamada);
+var cargador1 = new THREE.TextureLoader();
+  cargador1.load("MarmolGris.jpg", AJEDREZ.retrollamada1);
+var cargador2 = new THREE.TextureLoader();
+  cargador2.load("marmolcafe.jpg", AJEDREZ.retrollamada2);
+var cargador3 = new THREE.TextureLoader();
+  cargador3.load("MarmolBlanco.jpg", AJEDREZ.retrollamada3);
+var cargador4 = new THREE.TextureLoader();
+  cargador4.load("MarmolGris.jpg", AJEDREZ.retrollamada4);
 AJEDREZ.escena.add(AJEDREZ.luzPuntual);
 AJEDREZ.luzPuntual.castShadow = true;
-  
+
+   
 }
 
 AJEDREZ.loop = function(){
