62be6c7cd5b06293f074c26cc5072d0473b96564
rickenbacker920402 <r.ivan02@gmail.com>
2016-10-20 15:23:00 -0500

    Update objeto3D.js

--- rv/objeto3D.js
+++ rv/objeto3D.js
@@ -4,7 +4,7 @@ THREE.Object3D.call(this);
 this.piernaIzq = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
 this.piernaDer = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
 var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
-this.add(this.piernaIzq, this.piernaDer, cuerpo);
+add(this.piernaIzq, this.piernaDer, cuerpo);
 this.piernaIzq.position.z = -2;
 this.piernaIzq.position.y = -2.5;
 this.piernaDer.position.z = 2;
