a2ffce472b4bcd99cf6cd53bd217569340611d92
rickenbacker920402 <r.ivan02@gmail.com>
2016-10-20 15:02:01 -0500

    Create objeto3D.js

---
+++ rv/objeto3D.js
@@ -0,0 +1,33 @@
+function Pieza(){
+THREE.Object3D.call(this);
+this.piernaIzq = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+this.piernaDer = new THREE.Mesh(new THREE.BVoxGeometry(1,5,1));
+var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
+tyhis.add(piernaIzq,piernaDer,cuerpo);
+this.piernaIzq.position.z = -2;
+this.piernaIzq.position.y = -2.5;
+this.piernaDer.position.z = 2;
+this.piernaDer.position.y =-2.5;
+cuerpo.position.z = 2.5;
+}
+var pieza;
+Pieza.prototupe = new THREE.Object3D;
+
+function setup(){
+pieza = new Pieza();
+camara = new THREE.PerspectiveCamera();
+camara.position.z = 20;
+
+var lienzo = document.getElementById("ejemplo-3d");
+renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+renderizador.setSize(600,600);
+
+escena = new THREE.Scene();
+escena.add(pieza);
+}
+function loop(){
+requestAnimationFrame(PROTOTIPO.loop);
+pieza.rotateY=0.1;
+PROTOTIPO.renderizador.render(PROTOTIPO.escena,PROTOTIPO.camara);
+}
+
