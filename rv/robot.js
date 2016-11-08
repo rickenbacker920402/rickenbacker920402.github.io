047ebf444f782f73a5329fe8df5329b93b345ffd
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-08 15:12:58 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -46,10 +46,10 @@ Wall.prototype = new THREE.Mesh();
 Environment.prototype.setMap = function(map){
 var _offset = Math.floor(map.length/2);
 
-for (var i= 0; i< map.length ; i++)
+for (var i= 0; i< map.length ; i++){
 for (var j= 0; j < map.length; j++){
 if (map[i][j] === "x")
-this.add(new Wall(1, j - offset, -(i-_offset)));
+this.add(new Wall(1, j - _offset, -(i-_offset)));
 else if (map[i][j] === "r")
 this.add( new Robot(0.5, j -_offset, -(i-_offset)));
 }
