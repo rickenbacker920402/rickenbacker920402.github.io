6fa01bc3fb85704d824913589d8b51bd903fcaa2
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-08 15:35:01 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -52,7 +52,7 @@ for (var j= 0; j < map.length; j++){
 if (map[i][j] === "x")
 this.add(new Wall(1, j - _offset, -(i-_offset)));
 else if (map[i][j] === "r")
-this.add( new Robot(0.5, j -_offset, -(i-_offset)));
+this.add( new Robot(0.5, j - _offset, -(i - _offset)));
 }
 }
 }
