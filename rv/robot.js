bc1e7d69852959cf3384ccd6ee20ace5a29ac29b
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-08 15:30:45 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -37,7 +37,7 @@ this.children[i].act(this);
 }
 
 function Wall(size, x, y){
-three.Mesh.call(this, new THREE.BoxGeometry(size, size, size), new THREE.MeshNormalMaterial());
+THREE.Mesh.call(this, new THREE.BoxGeometry(size, size, size), new THREE.MeshNormalMaterial());
 this.size = size;
 this.position.x = x;
 this.position.y = y;
