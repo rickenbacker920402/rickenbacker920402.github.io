ac47ba9608f9655a910d50a4e75de76482e0afe1
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-20 01:43:46 -0600

    Update ajedrezm.js

--- rv/ajedrezm.js
+++ rv/ajedrezm.js
@@ -109,7 +109,7 @@ camara = new THREE.PerspectiveCamera(campoVision,
                                           planoCercano,
                                           planoLejano);
 camara.position.z = 230;
-camara.position.y = -30;
+//camara.position.y = -30;
 camara.lookAt(new THREE.Vector3(0,0,0));
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerWidth, window.innerHeight);
