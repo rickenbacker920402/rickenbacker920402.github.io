d25c6436ae4721920a3cca4bbe4b578d8f6a3e2a
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-19 23:11:10 -0600

    Update ajedrezm.js

--- rv/ajedrezm.js
+++ rv/ajedrezm.js
@@ -41,27 +41,30 @@ this.children[i].act(this);
 function Borde(size, x, y, textura){
 var textura1 = new THREE.TextureLoader();
 THREE.Mesh.call(this, new THREE.BoxGeometry(size, size, size), new THREE.MeshBasicMaterial({map: textura1.load("marmolcafe.jpg")}));
-this.size = 10;
+this.size = size;
 this.position.x = x;
 this.position.y = y;
+this.position.z = 0;
 }
 Borde.prototype = new THREE.Mesh();
 
 function EspacioGris(size, x, y){
 var textura2 = new THREE.TextureLoader();
 THREE.Mesh.call(this, new THREE.BoxGeometry(size, size, size), new THREE.MeshBasicMaterial({map: textura2.load("MarmolGris.jpg")}));
-this.size = 10;
+this.size = size;
 this.position.x = x;
 this.position.y = y;
+this.position.z = 0;
 }
 EspacioGris.prototype = new THREE.Mesh();
 
 function EspacioBlanco(size, x, y){
 var textura3 = new THREE.TextureLoader();
 THREE.Mesh.call(this, new THREE.BoxGeometry(size, size, size), new THREE.MeshBasicMaterial({map: textura3.load("MarmolBlanco.jpg")}));
-this.size = 10;
+this.size = size;
 this.position.x = x;
 this.position.y = y;
+this.position.z = 0;
 }
 EspacioBlanco.prototype = new THREE.Mesh();
 
@@ -71,11 +74,11 @@ var _offset = Math.floor(map.length/2);
 for (var i= 0; i< map.length ; i++){
 for (var j= 0; j < map.length; j++){
 if (map[i][j] === "x")
-this.add(new Borde(1, j - _offset, -(i-_offset)));
+this.add(new Borde(10, j - _offset, -(i-_offset)));
 else if (map[i][j] === "y")
-this.add( new EspacioGris(1, j - _offset, -(i - _offset)));
+this.add( new EspacioGris(10, j - _offset, -(i - _offset)));
  else if (map[i][j] === "z")
-this.add( new EspacioBlanco(1, j - _offset, -(i - _offset)));
+this.add( new EspacioBlanco(10, j - _offset, -(i - _offset)));
 }
 }
 }
