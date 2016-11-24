5871939ccdab0932df65f11ea6f12adaec5f99be
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-24 15:28:28 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -3,11 +3,14 @@ var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
 var base3 = new THREE.CylinderGeometry(0.5, 0.6,0.4,50,25);
 var base4 = new THREE.TorusGeometry( 0.45, 0.1, 16, 100 );
 var base5 = new THREE.CylinderGeometry(0.3, 0.45,1,50,25);
+var base6 = new THREE.TorusGeometry( 0.45, 0.3, 16, 100 );
 
 
 
 base2.rotateX(Math.PI/2);
 base4.rotateX(Math.PI/2);
+base6.rotateX(Math.PI/2);
+
 
 
 
@@ -16,6 +19,7 @@ base2.translate(0, -0.9, 0);
 base3.translate(0, -0.6, 0);
 base4.translate(0, -0.4, 0);
 base5.translate(0, 0.2, 0);
+base6.translate(0, 0.8, 0);
 
 
 
@@ -25,6 +29,8 @@ var base2Malla = new THREE.Mesh(base2);
 var base3Malla = new THREE.Mesh(base3);
 var base4Malla = new THREE.Mesh(base4);
 var base5Malla = new THREE.Mesh(base5);
+var base6Malla = new THREE.Mesh(base6);
+
 
 
 var torreForma = new THREE.Geometry();
@@ -34,6 +40,7 @@ torreForma.merge(base2Malla.geometry,base2Malla.matrix);
 torreForma.merge(base3Malla.geometry,base3Malla.matrix);
 torreForma.merge(base4Malla.geometry,base4Malla.matrix);
 torreForma.merge(base5Malla.geometry,base5Malla.matrix);
+torreForma.merge(base6Malla.geometry,base6Malla.matrix);
 
 
 
