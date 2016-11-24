88846d30dcffde4edaa48463bab34e33963292da
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-24 15:09:20 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -1,22 +1,26 @@
 var base = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25);
 var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
 var base3 = new THREE.CylinderGeometry(0.5, 0.6,0.4,50,25);
+var base4 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
 
 
 
 base2.rotateX(Math.PI/2);
+base4.rotateX(Math.PI/2);
 
 
 
 base.translate(0, -1, 0);
 base2.translate(0, -0.9, 0);
 base3.translate(0, -0.6, 0);
+base4.translate(0, -0.2, 0);
 
 
 
 var baseMalla = new THREE.Mesh(base);
 var base2Malla = new THREE.Mesh(base2);
 var base3Malla = new THREE.Mesh(base3);
+var base4Malla = new THREE.Mesh(base4);
 
 
 var torreForma = new THREE.Geometry();
@@ -24,6 +28,8 @@ var torreForma = new THREE.Geometry();
 torreForma.merge(baseMalla.geometry,baseMalla.matrix);
 torreForma.merge(base2Malla.geometry,base2Malla.matrix);
 torreForma.merge(base3Malla.geometry,base3Malla.matrix);
+torreForma.merge(base4Malla.geometry,base4Malla.matrix);
+
 
 
 
