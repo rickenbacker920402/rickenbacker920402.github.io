6b9e0776c8bb4f3a565ce8da15732981151c9185
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-24 15:51:40 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -7,7 +7,7 @@ var base6 = new THREE.TorusGeometry( 0.3, 0.15, 16, 100 );
 var base7 = new THREE.CylinderGeometry(0.5, 0.35,0.4,50,25);
 var base8 = new THREE.TorusGeometry( 0.4, 0.1, 16, 100 );
 var base9 = new THREE.SphereGeometry( 0.4, 32,32 );
-var base10 = new THREE.SphereGeometry( 0.05, 32,32 );
+var base10 = new THREE.SphereGeometry( 0.1, 32,32 );
 
 
 
