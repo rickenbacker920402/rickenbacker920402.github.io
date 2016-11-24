733a0ebee4b2241542fbdb387f5b9ba0642c818a
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-24 15:06:43 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -1,6 +1,6 @@
 var base = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25);
 var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
-var base3 = new THREE.CylinderGeometry(0.6, 0.7,0.4,50,25);
+var base3 = new THREE.CylinderGeometry(0.5, 0.6,0.4,50,25);
 
 
 
