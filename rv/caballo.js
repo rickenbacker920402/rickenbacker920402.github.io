8d29abe23e45ba07fe5251c9dceb8f737a99c270
rickenbacker920402 <r.ivan02@gmail.com>
2016-11-28 13:53:28 -0600

    Update caballo.js

--- rv/caballo.js
+++ rv/caballo.js
@@ -1,7 +1,7 @@
 var base = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25);
 var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
 var base3 = new THREE.BoxGeometry(0.4,1.5,0.7);
-//
+
 
 
 base2.rotateX(Math.PI/2);
