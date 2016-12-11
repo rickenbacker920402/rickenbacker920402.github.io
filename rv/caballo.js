8d2f0440b33c60a5b59bc12d365e2756fab73294
rickenbacker920402 <r.ivan02@gmail.com>
2016-12-11 16:43:01 -0600

    Update caballo.js

--- rv/caballo.js
+++ rv/caballo.js
@@ -2,6 +2,7 @@ var base = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25);
 var base2 = new THREE.TorusGeometry( 0.5, 0.2, 16, 100 );
 var base3 = new THREE.BoxGeometry(0.7,1.5,0.5);
 var base4 = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 50, 25, false,5.5,5);
+var base5 = new THREE.ConeGeometry( 0.1, 0.2, 32 );
 
 //
 
@@ -13,12 +14,13 @@ base.translate(0, -1, 0);
 base2.translate(0, -0.9, 0);
 base3.translate(0,-0.4,0);
 base4.translate(0.2,0.3,0);
+base5.translate(0,0.5,0.25);
 
 var baseMalla = new THREE.Mesh(base);
 var base2Malla = new THREE.Mesh(base2);
 var base3Malla = new THREE.Mesh(base3);
 var base4Malla = new THREE.Mesh(base4);
-//var base5Malla = new THREE.Mesh(base5);
+var base5Malla = new THREE.Mesh(base5);
 //var base6Malla = new THREE.Mesh(base6);
 //var base7Malla = new THREE.Mesh(base7);
 //var base8Malla = new THREE.Mesh(base8);
@@ -33,7 +35,7 @@ torreForma.merge(baseMalla.geometry,baseMalla.matrix);
 torreForma.merge(base2Malla.geometry,base2Malla.matrix);
 torreForma.merge(base3Malla.geometry,base3Malla.matrix);
 torreForma.merge(base4Malla.geometry,base4Malla.matrix);
-//torreForma.merge(base5Malla.geometry,base5Malla.matrix);
+torreForma.merge(base5Malla.geometry,base5Malla.matrix);
 //torreForma.merge(base6Malla.geometry,base6Malla.matrix);
 //torreForma.merge(base7Malla.geometry,base7Malla.matrix);
 //torreForma.merge(base8Malla.geometry,base8Malla.matrix);
